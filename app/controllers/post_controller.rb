1149e024ca0baa97fde5146ab2ea0ad8740bb664
Cody Brooks <codyjamesbrooks@gmail.com>
2021-09-15 10:42:48 -0600

    created a nav bar partial

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,24 @@
 class PostController < ApplicationController
+  def index
+   @posts = Post.all 
+  end 
+
   def new
     @post = Post.new
   end
 
   def create
-    render plain: params
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to all_posts_path
+    else
+      redirect_to new_post_path
+    end 
   end
+
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end 
 end
